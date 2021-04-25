840e8bae43dd0a52b6c062954e54033399424bb0
DaniilKurandin <kurandin1995@mail.ru>
2021-04-25 15:53:40 +0500

    MASTER-1: start add professional_homework4

--- src/main/java/proffesional_homework4/task3/Main.java
+++ src/main/java/proffesional_homework4/task3/Main.java
@@ -1,37 +1,21 @@
 package proffesional_homework4.task3;
 
 import java.io.*;
-import java.util.Arrays;
-
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (OutputStream os = new FileOutputStream("D:\\java_test\\test2.txt");
-             InputStream is = new FileInputStream("D:\\java_test\\test2.txt")) {
-
-            int[] arr = new int[10];
-
-            for (int i = 0; i < arr.length; i++) {
-                arr[i] = (int) (Math.random() * 100);
-            }
-
-            Arrays.sort(arr);
-
-            for (int i = 0; i < arr.length; i++) {
-                os.write(arr[i]);
-            }
-
-            os.flush();
-
-            int size = is.available();
-
-            for (int i = 0; i < size; i++) {
-                System.out.print(is.read() + " ");
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        Scanner scanner = new Scanner(new File("D:\\java_test\\test2.txt"));
+        List<Integer> arr = new ArrayList<>();
+        while (scanner.hasNext()) {
+            arr.add(scanner.nextInt());
         }
 
+        Collections.sort(arr);
+
+        System.out.println(arr);
+
+        scanner.close();
     }
+
 }
