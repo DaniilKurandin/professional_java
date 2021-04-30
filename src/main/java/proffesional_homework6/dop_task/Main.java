42bdfc13dfcaaa79592bdf20eadff0ec762ed362
DaniilKurandin <kurandin1995@mail.ru>
2021-04-30 14:02:02 +0500

    MASTER-1: start add professional_homework6

---
+++ src/main/java/proffesional_homework6/dop_task/Main.java
@@ -0,0 +1,38 @@
+package proffesional_homework6.dop_task;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String login;
+        String password;
+
+        while (true) {
+            System.out.println("login: ");
+            if (sc.hasNext("[a-zA-Z]+")) {
+               login = sc.nextLine();
+            } else {
+                System.out.println("invalid username");
+                sc.nextLine();
+                continue;
+            }
+            System.out.println("password: ");
+            if (sc.hasNext("[a-z-A-Z0-9]+")) {
+                password = sc.nextLine();
+            } else {
+                System.out.println("invalid password");
+                sc.nextLine();
+                continue;
+            }
+            break;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("login: ").append(login).append("\n").append("password: ").append(password);
+        System.out.println(sb.toString());
+
+    }
+}
