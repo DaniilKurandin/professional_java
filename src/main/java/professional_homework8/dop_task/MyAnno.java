ef5d42537a91a7638e73561ded92f8049b25a220
DaniilKurandin <kurandin1995@mail.ru>
2021-05-04 15:46:36 +0500

    MASTER-1: start add professional_homework8

--- src/main/java/professional_homework8/dop_task/MyAnno.java
+++ src/main/java/professional_homework8/dop_task/MyAnno.java
@@ -1,6 +1,10 @@
 package professional_homework8.dop_task;
 
 import java.lang.annotation.*;
+
+
+//@Target(ElementType.METHOD)
+//@Retention(RetentionPolicy.RUNTIME) не получается задать аннотации, ругается
 @Repeatable(MyAnno2.class)
 public @interface MyAnno {
     int num1() default 0;
--- src/main/java/professional_homework8/task2/Math.java
+++ src/main/java/professional_homework8/task2/Math.java
@@ -1,12 +1,10 @@
 package professional_homework8.task2;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Math {
     int n1() default 0;
     int n2() default 0;
